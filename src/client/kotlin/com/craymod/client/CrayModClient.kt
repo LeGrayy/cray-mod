12e888addc4d2830de55177b5d61b0bdac9a4552
Gray <lucasschaefer2006@gmail.com>
2026-06-30 14:53:07 +0200

    Initial commit

---
+++ src/client/kotlin/com/craymod/client/CrayModClient.kt
@@ -0,0 +1,9 @@
+package com.craymod.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object CrayModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
